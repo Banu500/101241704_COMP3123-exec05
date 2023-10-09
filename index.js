993b07d3baa540c73e74fb05b956ed445164c13f
Banu500 <101241704@georgebrown.ca>
2023-10-08 23:28:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-// Serve a static HTML file named home.html with a welcome message
+
 router.get('/home', (req, res) => {
   res.sendFile(__dirname + '/home.html');
 });
 
-// Simulate user data
 const userData = [
   { username: 'user1', password: 'password1' },
   { username: 'user2', password: 'password2' },
-  // Add more user data as needed
+
 ];
 
-// Modify /login route to accept username and password as query string parameters
-// and validate them against the userData array
+
 router.get('/login', (req, res) => {
   const { username, password } = req.query;
   const user = userData.find((u) => u.username === username);
@@ -29,7 +27,7 @@ router.get('/login', (req, res) => {
   }
 });
 
-// Modify /logout route to accept username as a parameter and display a message in HTML format
+
 router.get('/logout/:username', (req, res) => {
   const { username } = req.params;
   res.send(`<b>${username} successfully logged out.</b>`);
